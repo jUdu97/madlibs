c5b6448a4e5a6747939316a7b8a9a42a8a48864c
Joshua Uduehi <juduehi41@gmail.com>
2020-08-16 23:15:01 -0500

    completed stories

--- madlib.py
+++ madlib.py
@@ -1,1 +1,47 @@
-#create mad lib options for user to choose from
+#Create mad lib options for user to choose from
+story1 = """
+Get excited for the brand new submarine base, the S.S. {}.
+It has a 24 hour fitness center, unlimited dining, phenomenal views of
+underwater sealife, and over {} rooms, which can be used for lounging, {},
+entertainment, and dining.
+At about 100 miles below the surface and off the coast of {},
+the S.S. {} is a state of the art mobile conference center and vacation spot.
+Forget travelling to {} or {} for your summer vacation. Choose Yempton's
+Top Rated holiday get away tour for over {} years. Book your tickets today,
+start at ${}.99!
+"""
+story2 = """
+{} {} {} is a renowned {} in the field of {}.
+Born in {}, their biggeset contribution to {} was inventing {}.
+Growing up in {}, Nebraska enabled a helpful environment for them to foster
+their interest in {}.
+They attended {} University and majored in {} and then got their Masters degree
+in {} from the University of {}. After graduation, {} went on to make {}
+Enterprises, a middle size company of {} - {} people.
+"""
+
+story3 = """
+Beware of the Haunted House off the intersection of {} Street and {} Boulevard.
+The owners of the house, the {} family, sold if off years ago and moved East.
+I heard that after the pet {} died, the family was devastated. The week
+following the pet's death, the atmosphere of the house became {}:
+cabinets {}ing open and shut, random objects {}ing down, and chilling
+temperatures, despite the thermostat being set to {} degrees Fahrenheit.
+After a while, all these {} happenings grew too much and forced the family out.
+A new family has recently has been looking at {}ing the house, so good luck
+to them!
+"""
+
+story4 = """
+A HERO'S WELCOME! A new hero has emerged on the scene. Say hello to Captain {}.
+Noone where they came from, but they've sworn to protect {} City. They claim
+to have the coolest superpowers, like {}, {}, and {}. Their uniform chose the
+colors {} and {} to commemorate their lost world. In a statement to the public,
+Captain {} detailed their commitment to truth, liberty, and {} for all those
+who live in {} City.
+"""
+
+story5 = """
+In the Wild West, there are many legendary cowboys and outlaws. None compare to
+the person we'll discuss today. This is the ballad of {} {}. Originating and operating out of {}, Texas, {}. Renowned for their record-time {} skills, {} was hard to beat in a duel. With this reputation, {} traveled across the West Coast, winning duel after duel. Records indicate that over the course of over {} duels, {} {} only lost once. Eventually, the duels took their toll and {} allegedly retired to the {} region.
+"""
